556c17a1638035ad1b411cb704dcad44f3ba3326
赵兵 <zhaobing@xunlei.com>
2020-04-12 23:34:52 +0800

    feat(spider): 移除外部依赖

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import {isSpider} from '@/utils/util';
-
 /**
  *
  *
@@ -10,11 +8,11 @@ export default class NuxtFetchPipeline {
   constructor (
     {pipelines = {}, stages = {}} = {
       pipelines: {},
-      stages: {},
+      stages: {}
     }
   ) {
-    this.pipelines = pipelines;
-    this.stages = stages;
+    this.pipelines = pipelines
+    this.stages = stages
   }
 
   /**
@@ -25,20 +23,20 @@ export default class NuxtFetchPipeline {
    * @memberof NuxtFetchPipeline
    */
   runStageJobs (context, stage) {
-    const stageConfig = (this.stages && this.stages[stage]) || {};
-    const jobs = stageConfig.jobs;
+    const stageConfig = (this.stages && this.stages[stage]) || {}
+    const jobs = stageConfig.jobs
 
-    if (!jobs) return Promise.resolve;
+    if (!jobs) return Promise.resolve
 
     switch (stageConfig.type) {
       case 'parallel':
-        return Promise.all (
-          jobs.map (jobName => this.runJob (jobName, context))
-        );
+        return Promise.all(
+          jobs.map(jobName => this.runJob(jobName, context))
+        )
       case 'serial':
-        return jobs.reduce ((promise, jobName) => {
-          return promise.then (() => this.runJob (jobName, context));
-        }, Promise.resolve ());
+        return jobs.reduce((promise, jobName) => {
+          return promise.then(() => this.runJob(jobName, context))
+        }, Promise.resolve())
     }
   }
 
@@ -52,9 +50,9 @@ export default class NuxtFetchPipeline {
    */
   async runJob (jobName, context) {
     if (typeof jobName === 'string') {
-      return this.pipelines[jobName].task (context);
+      return this.pipelines[jobName].task(context)
     } else if (typeof jobName === 'object' && jobName.jobType === 'stage') {
-      return this.runStageJobs (context, jobName.name);
+      return this.runStageJobs(context, jobName.name)
     }
   }
 
@@ -65,52 +63,52 @@ export default class NuxtFetchPipeline {
    * @memberof NuxtFetchPipeline
    */
   createClientContext () {
-    const app = window.$nuxt;
-    const store = this.$store;
-    const route = this.$route;
-    const router = this.$router;
-    const params = this.$route.params;
-    const query = this.$route.query;
-    const error = window.$nuxt.error;
-    const next = location => app.router.push (location);
+    const app = window.$nuxt
+    const store = this.$store
+    const route = this.$route
+    const router = this.$router
+    const params = this.$route.params
+    const query = this.$route.query
+    const error = window.$nuxt.error
+    const next = location => app.router.push(location)
     const redirect = (status, path, query) => {
       if (!status) {
-        return;
+        return
       }
 
       // if only 1 or 2 arguments: redirect('/') or redirect('/', { foo: 'bar' })
-      let pathType = typeof path;
+      let pathType = typeof path
       if (
         typeof status !== 'number' &&
         (pathType === 'undefined' || pathType === 'object')
       ) {
-        query = path || {};
-        path = status;
-        pathType = typeof path;
-        status = 302;
+        query = path || {}
+        path = status
+        pathType = typeof path
+        status = 302
       }
       if (pathType === 'object') {
-        path = router.resolve (path).route.fullPath;
+        path = router.resolve(path).route.fullPath
       }
       // "/absolute/route", "./relative/route" or "../relative/route"
-      if (/(^[.]{1,2}\/)|(^\/(?!\/))/.test (path)) {
-        next ({
+      if (/(^[.]{1,2}\/)|(^\/(?!\/))/.test(path)) {
+        next({
           path,
           query,
-          status,
-        });
+          status
+        })
       } else {
-        path = formatUrl (path, query);
+        path = formatUrl(path, query)
 
         if (process.client) {
           // https://developer.mozilla.org/en-US/docs/Web/API/Location/replace
-          window.location.replace (path);
+          window.location.replace(path)
 
           // Throw a redirect error
-          throw new Error ('ERR_REDIRECT');
+          throw new Error('ERR_REDIRECT')
         }
       }
-    };
+    }
 
     return {
       app,
@@ -120,8 +118,8 @@ export default class NuxtFetchPipeline {
       params,
       query,
       error,
-      redirect,
-    };
+      redirect
+    }
   }
 }
 
@@ -135,14 +133,14 @@ export default class NuxtFetchPipeline {
 export function pipelineMixin (pipelineInstance) {
   return {
     mounted () {
-      const context = pipelineInstance.createClientContext.call (this);
-      pipelineInstance.runStageJobs (context, 'mounted');
+      const context = pipelineInstance.createClientContext.call(this)
+      pipelineInstance.runStageJobs(context, 'mounted')
 
-      requestIdleCallback (() => {
-        pipelineInstance.runStageJobs (context, 'idle');
-      });
-    },
-  };
+      requestIdleCallback(() => {
+        pipelineInstance.runStageJobs(context, 'idle')
+      })
+    }
+  }
 }
 
 /**
@@ -154,10 +152,10 @@ export function pipelineMixin (pipelineInstance) {
  * @returns
  */
 export async function adaptiveFetch (pipelineInstance, context) {
-  if (isSearchEngine (context)) {
-    return pipelineInstance.runStageJobs (context, 'seoFetch');
+  if (isSearchEngine(context)) {
+    return pipelineInstance.runStageJobs(context, 'seoFetch')
   } else {
-    return pipelineInstance.runStageJobs (context, 'minFetch');
+    return pipelineInstance.runStageJobs(context, 'minFetch')
   }
 }
 
@@ -168,7 +166,7 @@ export async function adaptiveFetch (pipelineInstance, context) {
  * @returns
  */
 function isSearchEngine (context) {
-  return isSpider (context.store.state.ua);
+  return isSpider(context.store.state.ua)
 }
 
 /**
@@ -179,33 +177,33 @@ function isSearchEngine (context) {
  * @return {string}
  */
 function formatUrl (url, query) {
-  let protocol;
-  const index = url.indexOf ('://');
+  let protocol
+  const index = url.indexOf('://')
   if (index !== -1) {
-    protocol = url.substring (0, index);
-    url = url.substring (index + 3);
-  } else if (url.startsWith ('//')) {
-    url = url.substring (2);
+    protocol = url.substring(0, index)
+    url = url.substring(index + 3)
+  } else if (url.startsWith('//')) {
+    url = url.substring(2)
   }
 
-  let parts = url.split ('/');
-  let result = (protocol ? protocol + '://' : '//') + parts.shift ();
+  let parts = url.split('/')
+  let result = (protocol ? protocol + '://' : '//') + parts.shift()
 
-  let path = parts.filter (Boolean).join ('/');
-  let hash;
-  parts = path.split ('#');
+  let path = parts.filter(Boolean).join('/')
+  let hash
+  parts = path.split('#')
   if (parts.length === 2) {
-    [path, hash] = parts;
+    [path, hash] = parts
   }
 
-  result += path ? '/' + path : '';
+  result += path ? '/' + path : ''
 
-  if (query && JSON.stringify (query) !== '{}') {
-    result += (url.split ('?').length === 2 ? '&' : '?') + formatQuery (query);
+  if (query && JSON.stringify(query) !== '{}') {
+    result += (url.split('?').length === 2 ? '&' : '?') + formatQuery(query)
   }
-  result += hash ? '#' + hash : '';
+  result += hash ? '#' + hash : ''
 
-  return result;
+  return result
 }
 
 /**
@@ -215,34 +213,40 @@ function formatUrl (url, query) {
  * @return {string}
  */
 function formatQuery (query) {
-  return Object.keys (query)
-    .sort ()
-    .map (key => {
-      const val = query[key];
+  return Object.keys(query)
+    .sort()
+    .map(key => {
+      const val = query[key]
       if (val == null) {
-        return '';
+        return ''
       }
-      if (Array.isArray (val)) {
-        return val.slice ().map (val2 => [key, '=', val2].join ('')).join ('&');
+      if (Array.isArray(val)) {
+        return val.slice().map(val2 => [key, '=', val2].join('')).join('&')
       }
-      return key + '=' + val;
+      return key + '=' + val
     })
-    .filter (Boolean)
-    .join ('&');
+    .filter(Boolean)
+    .join('&')
 }
 
 function requestIdleCallback () {
   return (window.requestIdleCallback ||
     function (cb) {
-      var start = Date.now ();
-      return setTimeout (function () {
-        cb ({
+      var start = Date.now()
+      return setTimeout(function () {
+        cb({
           didTimeout: false,
           timeRemaining: function () {
-            return Math.max (0, 50 - (Date.now () - start));
-          },
-        });
-      }, 1);
+            return Math.max(0, 50 - (Date.now() - start))
+          }
+        })
+      }, 1)
     })
-    .apply (window, arguments);
+    .apply(window, arguments)
+}
+
+function isSpider (ua) {
+  return /Baiduspider|Googlebot|360Spider|Sogou web spider|Sogou Push Spider|Sogou Pic Spider|yisouspider|bingbot|SogouWebspider|Sosospider|Yahoo! Slurp|msnbot|EasouSpider|JikeSpider|YandexBot|AhrefsBot|ezooms.bot|YodaoBot|iaskspider|Sosoimagespider/.test(
+    ua
+  )
 }
